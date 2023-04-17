923b8dcc49130b972f9c151ee3f7cc7b239258b2
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-17 18:20:48 -0300

    add placeholder values to the component

--- src/app/components/shopping-list/shopping-list.component.ts
+++ src/app/components/shopping-list/shopping-list.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Ingredient } from '../../models/ingredient/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent {
+  ingredients: Ingredient[] = [];
 
+  constructor() {
+    this.ingredients = [
+      new Ingredient('Apples', 5),
+      new Ingredient('Tomatoes', 10),
+    ];
+  }
 }
