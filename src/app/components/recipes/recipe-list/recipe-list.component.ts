f5f228cc286177abd6586fd5ab1e4b1f7effa7a2
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-17 17:47:53 -0300

    add ngfor to list recipes and some styling

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../../../models/recipe/recipe';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
+  recipes: Recipe[] = [];
 
+  constructor() {
+    this.recipes = [
+      new Recipe(
+        'Tasty Schnitzel',
+        'A super-tasty Schnitzel - just awesome!',
+        'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG'
+      ),
+      new Recipe(
+        'Tasty Schnitzel',
+        'A super-tasty Schnitzel - just awesome!',
+        'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG'
+      ),
+    ];
+  }
 }
