aab3fcc514c8565edb8397af7939bc7c67bbc730
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-19 12:16:15 -0300

    add dropdown directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,7 @@ import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './components/shopping-list/shopping-edit/shopping-edit.component';
 import { RecipesComponent } from './components/recipes/recipes.component';
+import { DropdownDirective } from './directives/dropdown.directive';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { RecipesComponent } from './components/recipes/recipes.component';
     ShoppingListComponent,
     ShoppingEditComponent,
     RecipesComponent,
+    DropdownDirective,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
