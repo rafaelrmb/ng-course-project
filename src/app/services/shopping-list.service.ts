1a934593cf8a67135ae95fb634b8567ef893860e
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-20 11:33:26 -0300

    add functionality to shopping list service

--- src/app/services/shopping-list.service.ts
+++ src/app/services/shopping-list.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from 'src/app/models/ingredient/ingredient.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ShoppingListService {
+  updatedList = new EventEmitter<Ingredient[]>();
 
-  constructor() { }
+  private ingredientsList: Ingredient[] = [
+    new Ingredient('Apples', 5),
+    new Ingredient('Tomatoes', 10),
+  ];
+
+  constructor() {}
+
+  addNewIngredients(newIngredients: Ingredient) {
+    this.ingredientsList.push(newIngredients);
+    this.updatedList.emit(this.ingredientsList.slice());
+  }
+
+  getIngredientsList() {
+    return this.ingredientsList.slice();
+  }
 }
