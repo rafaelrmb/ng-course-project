1aed6fae45eb38bf88dcc6cfdd20e57357cc8c38
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-20 10:50:38 -0300

    add recipe service provider

--- src/app/components/recipes/recipes.component.ts
+++ src/app/components/recipes/recipes.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 import { Recipe } from 'src/app/models/recipe/recipe.model';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css'],
+  providers: [RecipeService],
 })
 export class RecipesComponent {
   selectedRecipe!: Recipe;
