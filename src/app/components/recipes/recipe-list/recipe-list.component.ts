0864cb53effe5f48a4575855a0bd9c9c9fb323db
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-20 10:51:18 -0300

    refactor populate recipe list using service

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,37 +1,24 @@
-import { Component, Output, EventEmitter } from '@angular/core';
-import { Recipe } from '../../../models/recipe/recipe.model';
+import { RecipeService } from 'src/app/services/recipe.service';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Recipe } from 'src/app/models/recipe/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css'],
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit {
   @Output() selectedRecipe = new EventEmitter<Recipe>();
   recipes: Recipe[] = [];
 
-  constructor() {
-    this.recipes = [
-      new Recipe(
-        'Tasty Schnitzel',
-        'A super-tasty Schnitzel - just awesome!',
-        'https://upload.wikimedia.org/wikipedia/commons/7/72/Schnitzel.JPG'
-      ),
-      new Recipe(
-        'Chicken Alfredo',
-        'Creamy pasta dish with chicken and Alfredo sauce',
-        'https://bellyfull.net/wp-content/uploads/2021/02/Chicken-Alfredo-blog-4-768x1024.jpg'
-      ),
-      new Recipe(
-        'Beef Stroganoff',
-        'Russian dish with sautéed beef, onions, and mushrooms in creamy sauce',
-        'https://lilluna.com/wp-content/uploads/2017/06/beef-stroganoff-final-resize-3.jpg'
-      ),
-    ];
-
+  constructor(private recipeService: RecipeService) {
     setTimeout(() => {
       this.selectedRecipe.emit(this.recipes[0]);
     }, 5000); //practice with spinners and ng template.
+  }
+
+  ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe) {
