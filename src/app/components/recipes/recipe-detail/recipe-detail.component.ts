b123981ceeba50480859779198f6d8b346588137
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-20 12:03:57 -0300

    feature: add ingredients to the shopping list

--- src/app/components/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { Ingredient } from 'src/app/models/ingredient/ingredient.model';
 import { Recipe } from 'src/app/models/recipe/recipe.model';
+import { ShoppingListService } from 'src/app/services/shopping-list.service';
 
 @Component({
   selector: 'app-recipe-detail',
@@ -8,4 +10,13 @@ import { Recipe } from 'src/app/models/recipe/recipe.model';
 })
 export class RecipeDetailComponent {
   @Input('recipe') selectedRecipe!: Recipe;
+
+  constructor(private shoppingListService: ShoppingListService) {}
+
+  addToShoppingList(ingredientsList: Ingredient[]) {
+    for (const ingredient of ingredientsList) {
+      this.shoppingListService.addNewIngredients(ingredient);
+      console.log(ingredient);
+    }
+  }
 }
