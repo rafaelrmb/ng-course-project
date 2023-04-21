738fbdbaae0b095d49ae1ff6a288f0cf9063cced
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-21 10:08:21 -0300

    remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,10 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  currentPage: string = 'recipes';
-
-  togglePageDisplay(pageName: string) {
-    this.currentPage = pageName;
-  }
-}
+export class AppComponent {}
