70258522b87077a72f525d1f14b3da88320edd40
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-21 10:04:36 -0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,32 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RecipesComponent } from './components/recipes/recipes.component';
+import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
+import { RecipeDetailComponent } from './components/recipes/recipe-detail/recipe-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: RecipesComponent,
+  },
+  {
+    path: 'recipes',
+    component: RecipesComponent,
+    children: [
+      {
+        path: ':name',
+        component: RecipeDetailComponent,
+      },
+    ],
+  },
+  {
+    path: 'shopping-list',
+    component: ShoppingListComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
