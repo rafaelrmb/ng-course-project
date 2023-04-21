c8723b9056f875a58c8da2826f245c0acc7a031a
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-21 10:33:35 -0300

    refactor code to use routing instead of input

--- src/app/components/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/components/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,4 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { RecipeService } from './../../../services/recipe.service';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Ingredient } from 'src/app/models/ingredient/ingredient.model';
 import { Recipe } from 'src/app/models/recipe/recipe.model';
 import { ShoppingListService } from 'src/app/services/shopping-list.service';
@@ -8,14 +10,25 @@ import { ShoppingListService } from 'src/app/services/shopping-list.service';
   templateUrl: './recipe-detail.component.html',
   styleUrls: ['./recipe-detail.component.css'],
 })
-export class RecipeDetailComponent {
-  @Input('recipe') selectedRecipe!: Recipe;
+export class RecipeDetailComponent implements OnInit {
+  recipe!: Recipe;
 
-  constructor(private shoppingListService: ShoppingListService) {}
+  constructor(
+    private shoppingListService: ShoppingListService,
+    private recipeService: RecipeService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   addToShoppingList(ingredientsList: Ingredient[]) {
     for (const ingredient of ingredientsList) {
       this.shoppingListService.addNewIngredients(ingredient);
     }
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe((params) => {
+      this.recipe = this.recipeService.getRecipesByName(params['name']);
+    });
   }
 }
