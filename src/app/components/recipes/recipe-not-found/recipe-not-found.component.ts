0bd73e12993cb8f60043b38a2bfac2562cd9300e
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-21 10:44:24 -0300

    add a mock function to simulate data loading

--- src/app/components/recipes/recipe-not-found/recipe-not-found.component.ts
+++ src/app/components/recipes/recipe-not-found/recipe-not-found.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { RecipeService } from 'src/app/services/recipe.service';
 
 @Component({
   templateUrl: './recipe-not-found.component.html',
-  styleUrls: ['./recipe-not-found.component.css']
+  styleUrls: ['./recipe-not-found.component.css'],
 })
 export class RecipeNotFoundComponent {
+  constructor(private router: Router, private recipeService: RecipeService) {}
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.router.navigate([
+        '/recipes',
+        this.recipeService.getRecipes()[0].name,
+      ]);
+    }, 3000);
+  }
 }
