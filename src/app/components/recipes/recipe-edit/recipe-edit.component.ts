caeedfad946c0e9c154f33d3f8e8315360e60f4a
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-24 21:12:04 -0300

    add form to the page with working buttons

--- src/app/components/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/components/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Ingredient } from 'src/app/models/ingredient/ingredient.model';
 
 @Component({
   templateUrl: './recipe-edit.component.html',
@@ -7,12 +9,46 @@ import { ActivatedRoute, Params } from '@angular/router';
 })
 export class RecipeEditComponent implements OnInit {
   isEditing: boolean = false;
+  newRecipeForm!: FormGroup;
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
     this.route.params.subscribe((params: Params) => {
       this.isEditing = params['name'] != null;
     });
+
+    this.newRecipeForm = this.fb.group({
+      name: ['', Validators.required],
+      description: ['', Validators.required],
+      imagePath: [''],
+      ingredients: this.fb.array([], Validators.required),
+    });
+  }
+
+  createNewRecipe() {
+    console.log(this.newRecipeForm);
+  }
+
+  cancelCreating() {
+    this.isEditing = false;
+    this.router.navigate(['../'], { relativeTo: this.route });
+  }
+
+  addIngredients() {
+    const ingredientsGroup = this.fb.group({
+      name: ['', Validators.required],
+      amount: ['', Validators.required],
+    });
+
+    (this.newRecipeForm.get('ingredients') as FormArray).push(ingredientsGroup);
+  }
+
+  get ingredientControls() {
+    return (this.newRecipeForm.get('ingredients') as FormArray).controls;
   }
 }
