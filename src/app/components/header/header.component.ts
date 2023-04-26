adb745da6e908e2a7c54d62426708a60c74b7903
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-26 19:20:44 -0300

    add fetching data on init

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,17 @@
 import { DataStorageService } from './../../services/data-storage.service';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   constructor(private dataStorage: DataStorageService) {}
+
+  ngOnInit(): void {
+    this.fetchData();
+  }
 
   saveData() {
     this.dataStorage.storeRecipes();
