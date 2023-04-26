921322c79f68c0091ec5e76c41c4b1e68042aed1
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-26 18:27:10 -0300

    add storeRecipes method to save recipes to backend

--- src/app/services/data-storage.service.ts
+++ src/app/services/data-storage.service.ts
@@ -1,9 +1,21 @@
+import { RecipeService } from 'src/app/services/recipe.service';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class DataStorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
 
-  constructor() { }
+  storeRecipes() {
+    const recipes = this.recipeService.getRecipes();
+
+    this.http
+      .put(
+        'https://ng-complete-course-97a40-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe();
+  }
 }
