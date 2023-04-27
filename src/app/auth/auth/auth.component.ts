4a84ec577f7dd2e25f8f4b9213640dd795bdac1b
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-27 19:11:34 -0300

    add reactive form and validation

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.css'],
 })
-export class AuthComponent {
+export class AuthComponent implements OnInit {
   isLoginMode = false;
+  authForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirm: [''],
+    });
+  }
 
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
   }
+
+  onSubmit() {}
 }
