8babfa34067a547e4f97a223a04c8876ca563000
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-27 18:52:53 -0300

    add functionality to switch between register and login

--- src/app/auth/auth/auth.component.ts
+++ src/app/auth/auth/auth.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
-  styleUrls: ['./auth.component.css']
+  styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent {
+  isLoginMode = false;
 
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
 }
