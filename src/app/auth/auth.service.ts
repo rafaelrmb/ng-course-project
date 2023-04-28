6348e3e7179f7375ef29772f53b1fd65e5c666df
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-28 10:40:30 -0300

    refactor error handling code to auth service

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { API_KEY } from './api-key';
+import { catchError, throwError } from 'rxjs';
 
 interface AuthResponse {
   kind: string;
@@ -18,9 +19,21 @@ export class AuthService {
   constructor(private http: HttpClient) {}
 
   signUp(email: string, password: string) {
-    return this.http.post<AuthResponse>(
-      `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
-      { email, password, returnSecureToken: true }
-    );
+    return this.http
+      .post<AuthResponse>(
+        `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
+        { email, password, returnSecureToken: true }
+      )
+      .pipe(
+        catchError((errorRes) => {
+          let errorMsg = 'An error has occurred!';
+
+          if (!errorRes.error || !errorRes.error.error) {
+            return throwError(() => new Error(errorMsg));
+          }
+
+          return throwError(() => new Error(errorRes.error.error.message));
+        })
+      );
   }
 }
