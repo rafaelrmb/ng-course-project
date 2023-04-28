129d533b2717158ba1d18e65c7cb9222a24d0a1a
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-28 10:59:49 -0300

    refactor repeated code to handle error

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { API_KEY } from './api-key';
 import { catchError, throwError } from 'rxjs';
@@ -24,17 +24,7 @@ export class AuthService {
         `https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=${API_KEY}`,
         { email, password, returnSecureToken: true }
       )
-      .pipe(
-        catchError((errorRes) => {
-          let errorMsg = 'An error has occurred!';
-
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(() => new Error(errorMsg));
-          }
-
-          return throwError(() => new Error(errorRes.error.error.message));
-        })
-      );
+      .pipe(catchError(this.handleError));
   }
 
   login(email: string, password: string) {
@@ -43,16 +33,16 @@ export class AuthService {
         `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${API_KEY}`,
         { email, password, returnSecureToken: true }
       )
-      .pipe(
-        catchError((errorRes) => {
-          let errorMsg = 'An error has occurred!';
+      .pipe(catchError(this.handleError));
+  }
 
-          if (!errorRes.error || !errorRes.error.error) {
-            return throwError(() => new Error(errorMsg));
-          }
+  private handleError(errorRes: HttpErrorResponse) {
+    let errorMsg = 'An error has occurred!';
 
-          return throwError(() => new Error(errorRes.error.error.message));
-        })
-      );
+    if (!errorRes.error || !errorRes.error.error) {
+      return throwError(() => new Error(errorMsg));
+    }
+
+    return throwError(() => new Error(errorRes.error.error.message));
   }
 }
