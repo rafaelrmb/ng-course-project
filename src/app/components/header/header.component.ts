9cf56c93838ec944d147778848cbc15e0c5e5a92
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-04-28 11:38:23 -0300

    add user subscription and auth check to update ui

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,16 +1,27 @@
+import { Subscription } from 'rxjs';
+import { AuthService } from './../../auth/auth.service';
 import { DataStorageService } from './../../services/data-storage.service';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
-export class HeaderComponent implements OnInit {
-  constructor(private dataStorage: DataStorageService) {}
+export class HeaderComponent implements OnInit, OnDestroy {
+  private userSub!: Subscription;
+  isLoggedIn = false;
+
+  constructor(
+    private dataStorage: DataStorageService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit(): void {
     this.fetchData();
+    this.userSub = this.authService.user.subscribe((user) => {
+      this.isLoggedIn = !!user;
+    });
   }
 
   saveData() {
@@ -19,5 +30,9 @@ export class HeaderComponent implements OnInit {
 
   fetchData() {
     this.dataStorage.fetchRecipes().subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.userSub.unsubscribe();
   }
 }
