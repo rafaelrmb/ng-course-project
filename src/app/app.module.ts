18ca110bc9998c5b471f2b60b2388c4c55c0736e
Rafael Barbieri <rafaelmotermb@gmail.com>
2023-05-03 12:17:44 -0300

    change formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AppRoutingModule } from './app-routing.module';
 import { RecipesModule } from './components/recipes/recipes.module';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { HeaderComponent } from './components/header/header.component';
